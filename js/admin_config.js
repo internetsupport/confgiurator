d600f9f2242b63a0ccd20b5207b937a2312aee33
Jgip <email@internetsupport.de>
2018-11-01 13:46:17 +0100

    erste änderung

---
+++ js/admin_config.js
@@ -0,0 +1,236 @@
+/** admin config **/
+
+
+// datepicker
+$('[data-toggle="datepickerStart"]').datepicker({
+    offset: 0,
+    // date: startDate,
+    weekStart: 1,
+    format: 'dd.mm.yyyy',
+    // autoPick: true,
+    language: 'de-DE',
+    days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+    daysShort: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+    daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+    months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+    monthsShort: ["Jan.", "Feb.", "März", "April", "Mai", "Juni", "Juli", "Aug.", "Sept.", "Okt.", "Nov.", "Dez."],
+    autoHide: true
+});
+$('[data-toggle="datepickerEnd"]').datepicker({
+    offset: 0,
+    // date: new Date(Date.now() + 12096e5),
+    weekStart: 1,
+    format: 'dd.mm.yyyy',
+    // autoPick: true,
+    language: 'de-DE',
+    days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+    daysShort: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+    daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+    months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+    monthsShort: ["Jan.", "Feb.", "März", "April", "Mai", "Juni", "Juli", "Aug.", "Sept.", "Okt.", "Nov.", "Dez."],
+    autoHide: true
+});
+
+
+
+// datepicker
+$('[data-toggle="datepickerConfigThisYear"]').datepicker({
+    offset: 0,
+    weekStart: 1,
+    format: 'dd.mm.yyyy',
+    trigger: $('[data-toggle="datetriggerThisYear"]'),
+    autoPick: false,
+    language: 'de-DE',
+    days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+    daysShort: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+    daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+    months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+    monthsShort: ["Jan.", "Feb.", "März", "April", "Mai", "Juni", "Juli", "Aug.", "Sept.", "Okt.", "Nov.", "Dez."],
+    autoHide: true,
+    pick: addDateCalendar
+});
+$('[data-toggle="datepickerConfigEveryYear"]').datepicker({
+    offset: 0,
+    weekStart: 1,
+    format: 'dd.mm.',
+    trigger: $('[data-toggle="datetriggerEveryYear"]'),
+    autoPick: false,
+    language: 'de-DE',
+    days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+    daysShort: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+    daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+    months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+    monthsShort: ["Jan.", "Feb.", "März", "April", "Mai", "Juni", "Juli", "Aug.", "Sept.", "Okt.", "Nov.", "Dez."],
+    autoHide: true,
+    pick: addDateCalendar
+});
+
+// closing_days
+var output_this_year = $('#configDatesThisYear .add');
+var hidden_this_year = $("input[name='closing_days_new");
+var this_year_new;
+
+var input_this_year = $("input[name='closing_days").val();
+var string_this_year = input_this_year.replace(/"/g, '');
+var dates_this_year = string_this_year.split(',');
+
+function CALENDAR_DAYS_CLOSED_YEAR_init() {
+	this_year_new = dates_this_year.slice();
+
+	output_this_year.html(generatOutput(dates_this_year, 'js-thisYearDate'));
+}
+CALENDAR_DAYS_CLOSED_YEAR_init();
+
+
+function generatOutput (_dates, _class) {
+	var html = '', allDates;
+
+	if (_class == 'js-thisYearDate') {
+		allDates = merge_array(_dates, dates_this_year);
+	} else {
+		allDates = merge_array(_dates, dates_every_year);
+	}
+	var dates = sortDates(allDates);
+	// console.log("dates sort", dates);
+
+	for (var i = 0; i < dates.length; i++) {
+		var inArray = 0;
+		if (_dates.indexOf(dates[i]) != -1) {
+			inArray += 1;
+		}
+		if (_class == 'js-thisYearDate') {
+			if (dates_this_year.indexOf(dates[i]) != -1) {
+				inArray += 2;
+			}
+		} else {
+			if (dates_every_year.indexOf(dates[i]) != -1) {
+				inArray += 2;
+			}
+		}
+
+		var addClass = "";
+		if (inArray == 1) {
+			addClass = "configDate added delete";
+		}
+		if (inArray == 2) {
+			addClass = "configDate deleted";
+		}
+		if (inArray == 3) {
+			addClass = "configDate delete";
+		}
+		html += '<span class="' + _class + " " + addClass + '" data-value="' + dates[i] + '">' + dates[i] + ' <i class="fas fa-times removeDate"></i></span>';
+	}
+	return html;
+}
+
+// delete (triggert by class 'delete')
+$('.configDates').on('click', '.configDate.delete', function() {
+	var _this = $(this);
+	if (_this.hasClass('js-thisYearDate')) {
+		deleteDate(_this.attr('data-value'), this_year_new, output_this_year, 'js-thisYearDate');
+	}
+	if (_this.hasClass('js-everyYearDate')) {
+		deleteDate(_this.attr('data-value'), every_year_new, output_every_year, 'js-everyYearDate');
+	}
+});
+
+// re-add if deleted (triggert by class 'deleted')
+$('.configDates').on('click', '.configDate.deleted', function() {
+	var _this = $(this);
+	if (_this.hasClass('js-thisYearDate')) {
+		addDate(_this.attr('data-value'), this_year_new, output_this_year, 'js-thisYearDate', hidden_this_year);
+	}
+	if (_this.hasClass('js-everyYearDate')) {
+		addDate(_this.attr('data-value'), every_year_new, output_every_year, 'js-everyYearDate', hidden_every_year);
+	}
+});
+
+function deleteDate(_date, _array, _output, _class) {
+	var index = _array.indexOf(_date);
+	_array.splice(index, 1);
+	_output.html(generatOutput(_array, _class));
+	// save
+	if (_class == 'js-thisYearDate') {
+		hidden_this_year.val(_array.join());
+	} else {
+		hidden_every_year.val(_array.join());
+	}
+}
+function addDate (_newDate, _dateArray, _output, _class, _hiddenField) {
+	// console.log("addDate", _newDate, _dateArray, _output, _class, _hiddenField);
+	_dateArray.push(_newDate);
+	_output.html(generatOutput(_dateArray, _class));
+	_hiddenField.val(_dateArray.join());
+}
+
+function addDateCalendar (e) {
+	var elem = $(e.target);
+
+	var thisYear;
+	if (elem.parent().hasClass('js-thisYearDate')) {
+		thisYear = true;
+	} else {
+		thisYear = false;
+	}
+
+	if (e.view == 'day') {
+		var datum = getFormattedDate(e.date, thisYear);
+
+		if (thisYear) {
+			addDate(datum, this_year_new, output_this_year, 'js-thisYearDate', hidden_this_year);
+			// this_year_new.push(datum);
+			// output_this_year.html(generatOutput(this_year_new, 'js-thisYearDate'));
+			// hidden_this_year.val(this_year_new.join());
+		} else {
+			addDate(datum, every_year_new, output_every_year, 'js-everyYearDate', hidden_every_year);
+			// every_year_new.push(datum);
+			// output_every_year.html(generatOutput(every_year_new, 'js-everyYearDate'));
+			// hidden_every_year.val(every_year_new.join());
+		}
+	}	
+}
+
+function getFormattedDate(date, withYear) {
+     var year = date.getFullYear();
+     var month = (1 + date.getMonth()).toString();
+     month = month.length > 1 ? month : '0' + month;
+     var day = date.getDate().toString();
+     day = day.length > 1 ? day : '0' + day;
+
+     var returnString;
+     if (withYear) {
+     	returnString = day + "." + month + "." + year;
+     } else {
+     	returnString = day + "." + month + ".";
+     }
+     return returnString;
+}
+
+function sortDates (array) {
+	array.sort(function (a, b) {
+        a = a.toString().split('.');
+        b = b.toString().split('.');
+        return a[2] - b[2] || a[1] - b[1] || a[0] - b[0];
+    });
+    return array;
+}
+
+function merge_array(array1, array2) {
+	// console.log(array1, array2);
+    var result_array = [];
+    var arr = array1.concat(array2);
+    var len = arr.length;
+    var assoc = {};
+
+    while(len--) {
+        var item = arr[len];
+
+        if(!assoc[item]) 
+        { 
+            result_array.unshift(item);
+            assoc[item] = true;
+        }
+    }
+
+    return result_array;
+}
---
+++ jsonindex.php
@@ -0,0 +1,124 @@
+<?php
+// PHP Skripte von Jens Gippner, Internetsupport.de, März 2018 für TH
+$json_config_file = "config.json";
+
+class Misc_helper
+{
+   public static function clearinput($input) #returns $checked input;
+	{
+	$textstring = trim($input);
+	$textstring = strip_tags($textstring); // alle tags entfernen
+    $textstring =  htmlspecialchars($textstring,ENT_QUOTES);
+    #$textstring = str_replace("'", " ", $textstring);
+	$textstring = str_replace("|", " ", $textstring);
+	return $textstring;
+	}
+
+    public static function list_room_nr($input)
+    {
+    $list_room_options="";
+
+    for($i=0;$i<11;$i++)
+      {
+        if($i==$input) $list_room_options.="<option value=\"$i\" selected>$i</option>";
+        else $list_room_options.="<option value=\"$i\">$i</option>";
+      }
+
+    return $list_room_options;
+    }
+}
+
+
+$error_message="";
+$closing_days =  isset($_POST["closing_days_new"]) ? Misc_helper::clearinput($_POST["closing_days_new"]) : "";
+if($closing_days=="") $closing_days =  isset($_POST["closing_days"]) ? Misc_helper::clearinput($_POST["closing_days"]) : "";
+$closing_days_arr = explode(",",str_replace('&quot;','',$closing_days));
+
+#echo str_replace('&quot;','',$closing_days);
+$formdata =  isset($_POST["formdata"]) ? Misc_helper::clearinput($_POST["formdata"]) : 0;
+$room =  isset($_POST["room"]) ? Misc_helper::clearinput($_POST["room"]) : "";
+$email_address =  isset($_POST["email_address"]) ? Misc_helper::clearinput($_POST["email_address"]) : "";
+$user_name =  isset($_POST["user_name"]) ? Misc_helper::clearinput($_POST["user_name"]) : "";
+
+if($formdata=="1")
+{
+  $json_arr = array('closing_days_arr'=>$closing_days_arr,'room'=>$room,'email_address'=>$email_address,'user_name'=>$user_name);
+  $json_data = json_encode($json_arr);
+  file_put_contents($json_config_file, $json_data);
+  $error_message=" Daten erfolgreich gespeichert!";
+  echo "<br>error: ".json_last_error()."<br>$json_data";
+  $config_vars = "";
+
+}
+
+// get Date from jsonfile
+$json_file_data = file_get_contents($json_config_file);
+#$json_file_data = '{"closing_days_arr":["10.11.2018","11.11.2018","25.11.2018","28.11.2018"],"room":"5","email_address":"test@test.de","user_name":"Tester"}';
+
+$json_data_arr = json_decode($json_file_data, true);
+echo "<pre>";
+var_dump($json_data_arr);
+echo "</pre>";
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<title>Administration Konfiguration</title>
+		<!-- site style -->
+        <link rel="stylesheet" href="media/admin.css">
+        <link rel="stylesheet" href="js/datepicker/datepicker.min.css">
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src='js/datepicker/datepicker.min.js'></script>
+
+	</head>
+	<body>
+		<div id="main">
+		   <h1><i class="fas fa-cog fa-lg"></i> Administration Konfiguration</h1>
+			<form action="jsonindex.php" id="sf" name="formsb"  method="post">
+	  			<fieldset>
+	  				<legend>Schließtage</legend>
+	  				<input type="hidden" name="closing_days" value='<?=implode(",",$json_data_arr['closing_days_arr']);?>'>
+	  				<input type="hidden" name="closing_days_new" value="">
+                    <input type="hidden" name="formdata" value="1">
+	  				<label for="closing_days">Kalenderdatum</label>
+	  				<div class="spaceBeforeSmall configDates js-thisYearDate" id="configDatesThisYear">
+	  					<span class="add"></span>
+	  					<br>
+	  					<input class="hiddenDate" data-toggle="datepickerConfigThisYear"><span class="datetrigger" data-toggle="datetriggerThisYear"><i class="fas fa-plus"></i> Datum hinzufügen</span>
+	  				</div>
+
+	  			</fieldset>
+				<fieldset>
+	  				<legend>Raum</legend>
+	  				<label for="room">Raumnummer</label>
+	  				<select name="room" id="room" class="floatRight"><option value="">bitte auswählen</option><?php echo Misc_helper::list_room_nr($room);?></select>
+
+	  			</fieldset>
+				<fieldset>
+	  				<legend>E-Mail-Adresse</legend>
+	  				<label for="email_address">E-Mail</label> <input type="email" class="floatRight" size="25" maxlength="100" name="email_address" value="<?=$json_data_arr['email_address'];?>" id="email_address">
+
+	  			</fieldset>
+					<fieldset>
+	  				<legend>Name</legend>
+	  				<label for="user_name">Vor und Nachname</label> <input type="text" class="floatRight" size="25" maxlength="100" name="user_name" value="<?=$json_data_arr['user_name'];?>" id="user_name">
+	  	 	  			</fieldset>
+	  			<br>
+				<fieldset class="paddingArround">
+			  	<div class="flex flexSpaceBetween">
+                    <span><input type="submit" id="submitbutton" value="Speichern">
+                    <div class="red"><?=$error_message;?></div>
+			        </span>
+			  	</div>
+				</fieldset>
+			</form>
+			<br>
+		</div>
+		<!-- site script -->
+		<script src="js/admin.js"></script>
+		<script src="js/admin_config.js"></script>
+	</body>
+</html>
